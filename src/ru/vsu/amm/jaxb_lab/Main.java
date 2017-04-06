b2f5ee8ef0d2901071eefca8b58c525b095214f4
Nimtar <neemtar@gmail.com>
2017-04-06 17:14:19 +0300

    add and edit methods added.

--- src/ru/vsu/amm/jaxb_lab/Main.java
+++ src/ru/vsu/amm/jaxb_lab/Main.java
@@ -1,65 +1,92 @@
 package ru.vsu.amm.jaxb_lab;
 
-import javax.xml.bind.JAXBContext;
-import javax.xml.bind.JAXBException;
-import javax.xml.bind.Marshaller;
-import javax.xml.bind.Unmarshaller;
-import java.io.FileInputStream;
-import java.io.FileNotFoundException;
-import java.io.FileOutputStream;
 import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Main {
+    private static Scanner scanner = new Scanner(System.in);
+    private static Wrapper wrapper = getWrapper();
+
     public static void main(String[] args) {
-        try {
-            ArrayList<Student> studentList = new ArrayList<>();
-            Student student01 = new Student("Mitya", 21, 4, "phipsy", 973516);
-            Student student02 = new Student("Ilyusha", 20, 4, "amm", 873516);
-            Student student03 = new Student("Nikita", 21, 4, "amm", 873515);
-            studentList.add(student01);
-            studentList.add(student02);
-            studentList.add(student03);
-            Wrapper wrapper = new Wrapper();
-            wrapper.setStudents(studentList);
-            JAXBContext context = JAXBContext.newInstance(Wrapper.class);
-            Marshaller marshaller = context.createMarshaller();
-            marshaller.setProperty(Marshaller.JAXB_FORMATTED_OUTPUT, true);
-            marshaller.marshal(wrapper, new FileOutputStream("/home/nimtar/JavaLab/resourse.xml"));
-            Unmarshaller unmarshaller = context.createUnmarshaller();
-            Object student1 = unmarshaller.unmarshal(new FileInputStream("/home/nimtar/JavaLab/resourse.xml"));
-            System.out.println(student1);
-            String action = getAction();
-            while (!(action.equals("e") || action.equals("d") || action.equals("a"))) {
-                System.out.println("NO");
-                action = getAction();
+        StudentsMarshaller studentsMarshaller = new StudentsMarshaller();
+        studentsMarshaller.marshall(wrapper);
+        Object students1 = studentsMarshaller.unmarshall();
+        System.out.println(students1);
+        String action = getAction();
+        while (!action.equals("q")) {
+            Student student = null;
+            int index = -1;
+            if (action.equals("a") || (action.equals("e"))) {
+                student = getStudent();
             }
-            int index = getIndex();
-            while (index < 0 || index > wrapper.getStudents().size()) {
-                System.out.println("NOO..");
+            if (action.equals("d") || action.equals("e")) {
                 index = getIndex();
             }
             if (action.equals("d")) {
                 wrapper.delete(index);
             }
-
-
-        } catch (JAXBException | FileNotFoundException e) {
-            e.printStackTrace();
+            if (action.equals("a")) {
+                wrapper.add(student);
+            }
+            if (action.equals("e")) {
+                wrapper.edit(index, student);
+            }
+            studentsMarshaller.marshall(wrapper);
+            students1 = studentsMarshaller.unmarshall();
+            System.out.println(students1);
+            action = getAction();
         }
+
+    }
+
+    private static boolean isNotValid(String action) {
+        return !(action.equals("e") || action.equals("d") || action.equals("a"));
+    }
+
+    private static Wrapper getWrapper() {
+        ArrayList<Student> studentList = new ArrayList<>();
+        Student student01 = new Student("Mitya", 21, 4, "phipsy", 973516);
+        Student student02 = new Student("Ilyusha", 20, 4, "amm", 873516);
+        Student student03 = new Student("Nikita", 21, 4, "amm", 873515);
+        studentList.add(student01);
+        studentList.add(student02);
+        studentList.add(student03);
+        return new Wrapper().setStudents(studentList);
     }
 
     private static int getIndex() {
         System.out.println("Write down student's index");
-        Scanner scanner = new Scanner(System.in);
         int index = scanner.nextInt();
+        if (index < 0 || index > wrapper.getStudents().size()) {
+            System.out.println("NOO..");
+            index = getIndex();
+        }
         return index;
     }
 
-    private static String getAction() {
-        System.out.println("If you want to delete student press d, add - a, edit - e");
+    private static Student getStudent() {
+        System.out.println("Write down information about student. 1) Student's name: ");
         Scanner scanner = new Scanner(System.in);
+        String name = scanner.next();
+        System.out.println("2) Student's age: ");
+        int age = scanner.nextInt();
+        System.out.println("3) Student's year of studying: ");
+        int year = scanner.nextInt();
+        System.out.println("4) Student's faculty: ");
+        String faculty = scanner.next();
+        System.out.println("5) Student's id: ");
+        int id = scanner.nextInt();
+        return new Student(name, age, year, faculty, id);
+    }
+
+    private static String getAction() {
+        System.out.println("If you want to delete student press d, add - a, edit - e. " +
+                "\nIf you don't want to do anything press q");
         String action = scanner.next();
+        if (isNotValid(action)) {
+            System.out.println("NO");
+            getAction();
+        }
         return action;
     }
 
---
+++ src/ru/vsu/amm/jaxb_lab/StudentsMarshaller.java
@@ -0,0 +1,54 @@
+package ru.vsu.amm.jaxb_lab;
+
+import javax.xml.bind.*;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+
+public class StudentsMarshaller {
+    private Marshaller marshaller;
+    private Unmarshaller unmarshaller;
+
+
+    public StudentsMarshaller setMarshaller(Marshaller marshaller) {
+        this.marshaller = marshaller;
+        try {
+            marshaller.setProperty(Marshaller.JAXB_FORMATTED_OUTPUT, true);
+        } catch (PropertyException e) {
+            e.printStackTrace();
+        }
+        return this;
+    }
+
+    public StudentsMarshaller setUnmarshaller(Unmarshaller unmarshaller) {
+        this.unmarshaller = unmarshaller;
+        return this;
+    }
+
+    public void marshall(Wrapper wrapper) {
+        try {
+            marshaller.marshal(wrapper, new FileOutputStream("/home/nimtar/JavaLab/resourse.xml"));
+        } catch (JAXBException | FileNotFoundException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public Wrapper unmarshall() {
+        try {
+            return (Wrapper) unmarshaller.unmarshal(new FileInputStream("/home/nimtar/JavaLab/resourse.xml"));
+        } catch (JAXBException | FileNotFoundException e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
+
+    public StudentsMarshaller() {
+        try {
+            JAXBContext context = JAXBContext.newInstance(Wrapper.class);
+            this.setMarshaller(context.createMarshaller());
+            this.setUnmarshaller(context.createUnmarshaller());
+        } catch (JAXBException e) {
+            e.printStackTrace();
+        }
+    }
+}
